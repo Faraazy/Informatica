3392adefb202f055a409e5bfe62e12d25e224e36
Dylan Koster <148067.helenparkhurst.net>
2016-02-23 17:46:08 +0100

    achtergrond

--- Functies/Output.php
+++ Functies/Output.php
@@ -44,6 +44,12 @@ include 'Minnetjes_Naar_Letters.php';
 
     // $letters = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
            // );  
-?>       
+?> 
+    <style>    
+    div {
+    background-color: lightblue;
+}
+
+ </style>
 </body>
 </html>
